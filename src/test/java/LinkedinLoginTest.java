e5ad14f8875be16b95f1c52243e108f1a82270fb
CostaCore <konstantin.koresh@gmail.com>
2018-08-20 14:44:58 +0300

    Lesson 3 commit

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -7,6 +7,8 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
+import static java.lang.Thread.sleep;
+
 public class LinkedinLoginTest {
 
     @Test
@@ -18,61 +20,63 @@ public class LinkedinLoginTest {
         //click "Sing in' button
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.linkedin.com/");
-        String titlePage = "Войти или зарегист5рироваться";
-
+        String titlePage = "Войти или зарегистрироваться";
         String userEmail = "nsczxfxthntq@gmail.com";
         String userPassword = "4838960q";
-        String userName = "Costa Jo2nes";
-
+        String userName = "Costa Jones";
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/", "Page login URL is wrong!!!");
+        Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up", "Login page Title is wrong");
 
-        By titlePageLocator = By.xpath("//title[contains(text(),'LinkedIn: Войти или зарегистрироваться')]");
-        By singInButtonLocator = By.xpath("//input[@id='login-submit']");
-        By userEmailLocator = By.xpath("//*[@id='login-email']");
-        By userPasswordLocator = By.xpath("//*[@id='login-password']");
+        WebElement userEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement userPasswordField =driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
 
+        Assert.assertTrue(signInButton.isDisplayed(), "'Sign In' button is not displyed on Login Page(sting 35)");
 
-
-        String titlePageText = titlePageLocator.toString();
-        if (titlePageText.toLowerCase().contains(titlePage.toLowerCase()) == true) {
-            System.out.println("Title page is correct");
-        }else {
-            System.out.println("Page Title is not correct!!!");
-        }
-        List<WebElement> listSingInButton = driver.findElements(singInButtonLocator);
-        if (listSingInButton.size()==0){
-            System.out.println("'Sing In' button is not found!!!");
-        }
-        else {
-            WebElement singInButton = driver.findElement(singInButtonLocator);
-            System.out.println("'Sing In' button is found");
-            singInButton.click();
-            }
-
-
-    WebElement userEmailField = driver.findElement(userEmailLocator);
-        userEmailField.click();
-        userEmailField.clear();
         userEmailField.sendKeys(userEmail);
-
-        WebElement userPasswordField = driver.findElement(userPasswordLocator);
-        userPasswordField.click();
-        userPasswordField.clear();
         userPasswordField.sendKeys(userPassword);
+        signInButton.click();
 
-        WebElement singInButton = driver.findElement(singInButtonLocator);
-        singInButton.click();
+        WebElement profileNavItem = driver.findElement(By.xpath("//li[@id='profile-nav-item']"));
+        Assert.assertTrue(profileNavItem.isDisplayed(), "'Profile-nav-item ' button is not displyed on Login Page(sting 46)");
 
-        driver.manage().window().maximize();
-        Thread.sleep(5000);
 
-//        By userNameLocator = By.xpath("//*[@id='ember2089']/span");
-//        WebElement userName = driver.findElement(userNameLocator);
-//        System.out.println(driver.findElement(userNameLocator).getText());
     Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/feed/", "Home page not loaded");
     By userNameLocator = By.xpath("//a[@class='tap-target profile-rail-card__actor-link block link-without-hover-visited ember-view']/span");
     WebElement userNameField = driver.findElement(userNameLocator);
     Assert.assertEquals(userNameField.getText(), userName, "User name wrong");
+    }
+
+    @Test
+    public void negativeloginTest() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+        String titlePage = "Войти или зарегист5рироваться";
+        String userEmailWrong = "aa@l.com";
+        String userPasswordWrong = "wrongPass";
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/", "Page login URL is wrong!!!");
+        Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up", "Login page Title is wrong");
+
+        WebElement userEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement userPasswordField =driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
+
+        Assert.assertTrue(signInButton.isDisplayed(), "'Sign In' button is not displyed on Login Page(sting 35)");
+
+        userEmailField.sendKeys(userEmailWrong);
+        userPasswordField.sendKeys(userPasswordWrong);
+        signInButton.click();
+
+        try {
+            sleep(3000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        WebElement alertMessage = driver.findElement(By.xpath("//div[@role='alert']"));
+        Assert.assertEquals(alertMessage.getText(), "There were one or more errors in your submission. Please correct the marked fields below.", "Alert not working");
+
     }
  }
