255d2223ae8275996e25f8bf58a3f02dc6e00dd7
CostaCore <konstantin.koresh@gmail.com>
2018-09-17 16:53:56 +0300

    Lesson 10 commit add HomeWork switch/case in @Before Methods

--- src/test/java/test/LinkedinBaseTest.java
+++ src/test/java/test/LinkedinBaseTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import page.LinkedinLoginPage;
@@ -14,15 +15,34 @@ import page.LinkedinLoginPage;
  * Initializes linkedinLoginPage and WebDriver
  */
 public class LinkedinBaseTest {
-
     WebDriver driver;
     LinkedinLoginPage linkedinLoginPage;
 
     @BeforeMethod
     public void beforeMethod() {
-        WebDriverManager.firefoxdriver().setup();
+        String browserChrome = "chrome";
+        String browserFireFox = "firefox";
+        String browserIE = "IE";
 
-        driver = new FirefoxDriver();
+        String browserCurrent = browserChrome;
+
+        switch (browserCurrent){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            break;
+
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            break;
+            case "IE":
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+            break;
+            default: System.out.println("select nothing");
+        }
+
         driver.get("https://www.linkedin.com/");
         linkedinLoginPage = new LinkedinLoginPage(driver);
     }
